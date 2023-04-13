0e603b666b7e092b16946674fd8a8d74a8047b8f
김민서 <rlaalstj081711@gmail.com>
2023-04-13 06:23:37 +0000

    54,55,56,58

---
+++ src/54.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+   주석문
+   주석문
+*/
+int main(){
+	
+	int a, a1, 1a;
+	int A, cat, Cat, cAt, caT;
+	// 변수명은 숫자가 앞에 올수 없다.
+	int _a,__a,___;
+	int if;//예약어 변수명으로 사용할 수 없다.
+	
+	return 0;
+
+}
